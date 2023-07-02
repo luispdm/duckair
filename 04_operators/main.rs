ef0d17f8ff9f7fba99a02a5836e0304eb2a7a948
luispdm <17044119+luispdm@users.noreply.github.com>
2023-07-02 11:47:49 +0200

    operators

---
+++ 04_operators/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // math operators
+    println!("{}", 18%7); // modulus operator, prints 4
+    println!("{}, {}", i32::pow(8, 2), f32::powf(2.2, 2.0)); // 64 4.84
+    // println!("{}", i32::pow(2.2, 3)); // compiler error, need to use f32::powf for this operation
+
+    // logical operators & bitwise operators
+    // same as in go
+}
