6fc76085f9f4d30117ab31472a4fd0ad834b738b
luispdm <17044119+luispdm@users.noreply.github.com>
2023-07-12 16:43:40 +0200

    associative methods + suppress warnings

--- 09_data_structures/main.rs
+++ 09_data_structures/main.rs
@@ -1,3 +1,6 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
 struct Waypoint {
     name: String,
     latitude: f64,
@@ -9,14 +12,36 @@ struct Segment {
     end: Waypoint,
 }
 
+// in Rust, you don't write methods inside a struct, you write methods and you associate them this way
+// associative methods
+impl Segment {
+    fn new(start: Waypoint, end: Waypoint) -> Self {
+        Self {
+            start, end
+        }
+    }
+
+    fn distance(&self) -> f32 {
+        2513.0
+    }
+}
+
 fn main() {
-    let mut kcle = Waypoint {
+    // structs must also be declared mutable with the "mut" keyword
+    let kcle = Waypoint {
         name: "KCLE".to_string(),
         latitude: 41.4075,
         longitude: -81.851111
     };
-    let mut kslc = Waypoint {
+    let kslc = Waypoint {
         name: "KSLC".to_string(),
-        ..kcle // copying fields from kcle. "name" will be overridden by the statement above though
+        latitude: 40.7861,
+        longitude: -111.9822
     };
+    let kcle_copy = Waypoint {
+        name: "KCLE_COPY".to_string(),
+        ..kcle // copying fields from kcle. "name" will be overridden by the statement above
+    };
+    let seg = Segment::new(kcle, kslc);
+    println!("the distance between {} and {} is {:.1} km", seg.start.name, seg.end.name, seg.distance())
 }
