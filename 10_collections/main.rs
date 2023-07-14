cb92145b7762d3c2c691caa49245075c1d914a85
luispdm <17044119+luispdm@users.noreply.github.com>
2023-07-14 17:38:57 +0200

    collections & generics

---
+++ 10_collections/main.rs
@@ -0,0 +1,73 @@
+#![allow(unused_variables)]
+
+use std::collections::{HashMap, VecDeque, HashSet};
+
+fn main() {
+    /*
+     * Fixed-size collections are stored on the stack while resizable collections
+     * are stored on the heap.
+     *
+     * Vectors, linked lists and double ended queue vectors are called "sequences".
+     * Sequences store entries sequentially.
+     * Elements in linked lists and double-ended queue vectors can be added and
+     * removed from the front or the back of the list but cannot be sorted.
+     * The opposite is true for vectors
+     */
+
+    // two ways to declare a vector
+    let v = vec![1, 2, 3];
+    let mut mv: Vec<&str> = vec!["one"];
+    // four ways to add and remove elements
+    mv.push("two");
+    mv.push("three");
+    mv.push("four");
+    mv.pop();
+    mv.insert(3, "four");
+    mv.remove(3);
+    for i in mv.iter() {
+        println!("{}", i);
+    }
+    // 10 > mv.len() => Rust panics. Uncomment to trigger the error
+    // println!("{}", mv[10]);
+    /*
+     * Prefer "mv.get(index)". This is safer as it returns an Option.
+     * It is slightly slower than "mv[X]" but they are quite close in terms of performance,
+     * the difference might be noticed on millions-items vectors
+     */
+    // how to use the Option returned by mv.get? "if let" or "match"
+    if let Some(item) = mv.get(2) {
+        println!("{}", item); // "three"
+    }
+    let item = mv.get(10);
+    match item {
+        Some(item) => {
+            println!("{}", item)
+        }
+        _ => {
+            println!("element at index 10 not found")
+        }
+    }
+    // other useful methods are: clear(), contains(x) and len(), which do exactly what their names suggest
+    let mut vdq: VecDeque<String> = VecDeque::new();
+    vdq.push_back("two".to_string());
+    vdq.push_front("one".to_string()); // pop_front and pop_back exist too
+
+    /*
+     * Maps. Two generic types: one for the key and another for the value.
+     * Once the type is chosen it cannot be changed.
+     * Btree maps and hashmaps. In btree maps keys can be sorted.
+     */
+    let mut hm = HashMap::new();
+    hm.insert(1, ("1", "1"));
+    hm.insert(1, ("one", "onemap")); // no key collision checking => value at 1 overwritten
+    if let Some(item) = hm.get(&1) { // "get" works exactly as in vectors
+        println!("{:?}", item);
+    }
+
+    // sets store values only but they use a map to store data internally (key, map)
+    // btree sets and hashsets
+    let mut s = HashSet::new();
+    s.insert(1);
+    s.insert(1); // as with other languages, sets do not allow duplicates => element overwritten
+    println!("{:?}", s);
+}
---
+++ 11_generics/main.rs
@@ -0,0 +1,39 @@
+use std::ops::{Add, Mul};
+
+#[derive(Debug)]
+struct WithGenerics<T, U> {
+    name: String,
+    first: T,
+    second: U, // second's type can be the same as first or a different one
+}
+
+fn main() {
+    let test = WithGenerics {
+        name: "test".to_string(),
+        first: 1,
+        second: 2.2,
+    };
+    println!("{}, {}, {}", test.name, test.first, test.second);
+    println!("{}", add(1, 2));
+    println!("{}", add_and_multiply(2, 3));
+    println!("{:?}", dio(&test, &test));
+}
+
+// this tells the compiler that T must implement the trait "Add" in order to be used by the method
+fn add<T: Add<Output = T>>(a: T, b: T) -> T {
+    a + b
+}
+
+// when a type must implement multiple traits, prefer the "where" clause
+fn add_and_multiply<T>(a: T, b: T) -> T
+where
+    T: Add<Output = T> + Mul<Output = T> + Copy,
+{
+    let c = a + b;
+    c * a * b
+}
+
+// this is how you declare a method that uses both lifetime and generics
+fn dio<'a, T>(a: &'a T, _b: &T) -> &'a T {
+    a
+}
