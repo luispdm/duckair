32831dd10df3527d21fdb09c0e34ffae36a76cff
luispdm <17044119+luispdm@users.noreply.github.com>
2023-07-17 18:16:50 +0200

    threads comm

--- 12_concurrency/main.rs
+++ 12_concurrency/main.rs
@@ -1,4 +1,4 @@
-use std::thread;
+use std::{thread, sync::mpsc, sync::mpsc::{Receiver, Sender}};
 
 /*
  * Native threads and green threads
@@ -14,8 +14,9 @@ use std::thread;
  * for green threads
  */
 fn main() {
+    //threads basics
     let var = 412;
-    let jh = thread::spawn(move || {
+    let jh = thread::spawn(move || { // "move" because I need the closure to take ownership of var
         var * 2
     });
     // join ~= "waitGroup.wait()" in Go, plus it captures the return value coming from the thread
@@ -23,5 +24,41 @@ fn main() {
     match res {
         Ok(val) => {println!("{} * 2 = {}", var, val)},
         Err(_) => {println!("Thread panicked!")}
+    }
+
+    // threads communication - mpsc = multi-producer, single-consumer
+    let (alice_tx, alice_rx): (Sender<&str>, Receiver<&str>) = mpsc::channel();
+    let (bob_tx, bob_rx): (Sender<&str>, Receiver<&str>) = mpsc::channel();
+    let a_h = thread::spawn(move || {    
+        alice_chat(bob_tx, alice_rx)
+    });
+    let b_h = thread::spawn(move || {
+        bob_chat(alice_tx, bob_rx)
+    });
+    if a_h.join().is_ok() {}
+    if b_h.join().is_ok() {}
+}
+
+fn alice_chat(bob_tx:Sender<&str>, alice_rx:Receiver<&str>) {
+    match alice_rx.recv() {
+        Ok(res) => {
+            println!("Incoming message for Alice: {}", res);
+        }
+        Err(err) => {
+            println!("Cannot fetch message for Alice: {}", err);
+        }
+    }
+    let _send_msg = bob_tx.send("Hey Bob! How you doin'?!");
+}
+
+fn bob_chat(alice_tx:Sender<&str>, bob_rx:Receiver<&str>) {
+    let _send_msg = alice_tx.send("Hi Alice! It's a me, Bob");
+    match bob_rx.recv() {
+        Ok(res) => {
+            println!("Incoming message for Bob: {}", res);
+        }
+        Err(err) => {
+            println!("Cannot fetch message for Bob: {}", err);
+        }
     }
 }
