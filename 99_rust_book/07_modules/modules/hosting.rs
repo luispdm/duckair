929c2b2889240083b35dd005eace5373af4fd036
luispdm <17044119+luispdm@users.noreply.github.com>
2023-08-24 19:54:09 +0200

    pub use as

--- 99_rust_book/07_modules/modules/hosting.rs
+++ 99_rust_book/07_modules/modules/hosting.rs
@@ -1,3 +1,17 @@
+mod network {
+    /*
+     * This is how to export a property with a different name.
+     * Both the property and the statement must use "pub" to make
+     * the property accessible outside the module.
+     *
+     * "self" refers to the current module.
+     */
+    pub use self::godaddy::NAME as daddy;
+    mod godaddy {
+        pub const NAME: &str = "GO_DADDY";
+    }
+}
+
 pub fn hosting() {
-    println!("hosting");
+    println!("hosting {}", network::daddy);
 }
