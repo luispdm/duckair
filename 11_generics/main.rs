f535ef448634ef888b6f25e846d2208817408ccc
luispdm <17044119+luispdm@users.noreply.github.com>
2023-08-31 23:49:10 +0200

    rust book generics

--- 11_generics/main.rs
+++ 11_generics/main.rs
@@ -7,18 +7,6 @@ struct WithGenerics<T, U> {
     second: U, // second's concrete type can be the same as first or a different one
 }
 
-fn main() {
-    let test = WithGenerics {
-        name: "test".to_string(),
-        first: 1,
-        second: 2.2,
-    };
-    println!("{}, {}, {}", test.name, test.first, test.second);
-    println!("{}", add(1, 2));
-    println!("{}", add_and_multiply(2, 3));
-    println!("{:?}", lt_gn(&test, &test));
-}
-
 // this tells the compiler that T must implement the trait "Add" in order to be used by the method
 fn add<T: Add<Output = T>>(a: T, b: T) -> T {
     a + b
@@ -36,4 +24,83 @@ where
 // this is how you declare a method that uses both lifetime and generics
 fn lt_gn<'a, T>(a: &'a T, _b: &T) -> &'a T {
     a
+}
+
+/*
+ * Generics identifiers defined here don't need to be the same as the ones
+ * defined in the original struct (note X and Y here, while T and U were defined above).
+ */
+impl<X, Y> WithGenerics<X, Y> {
+    fn first(&self) -> &X {
+        &self.first
+    }
+    fn second(&self) -> &Y {
+        &self.second
+    }
+}
+
+// this method can be used only if "first" is a bool
+impl<U> WithGenerics<bool, U> {
+    fn no_generic(&self) -> bool {
+        self.first
+    }
+}
+
+// this is possible too but the generic types must be respected
+impl<T, U> WithGenerics<T, U> {
+    fn mixup<V, W>(self, another: WithGenerics<V, W>) -> WithGenerics<T, W> {
+        WithGenerics {
+            name: "i am quirky".to_string(),
+            first: self.first, // another.first wouldn't work
+            second: another.second, // self.second wouldn't work
+        }
+    }
+}
+
+// generics apply to enums too
+enum TheResult<T, E> {
+    Ok(T),
+    Err(E),
+}
+
+/*
+ * Generics in Rust do not cause performance penalties.
+ * How? Consider the following code:
+ * 
+ * enum Test<T> {
+ *   Value(T),
+ *   NoVal
+ * }
+ * 
+ * fn main() {
+ *   let integer = Test::Value(5);
+ *   let float = Test::Value(5.0);
+ * }
+ * 
+ * At compile time, Rust will turn the generic enum into 2 enums:
+ * enum Test_i32 {
+ *   Value(i32),
+ *   NoVal
+ * }
+ * enum Test_f64 {
+ *   Value(f64),
+ *   NoVal
+ * }
+ * 
+ * fn main() {
+ *   let integer = Test_i32::Value(5);
+ *   let float = Test_f64::Value(5.0);
+ * }
+ */
+
+fn main() {
+    let test = WithGenerics {
+        name: "test".to_string(),
+        first: 1,
+        second: 2.2,
+    };
+    println!("{}, {}, {}", test.name, test.first, test.second);
+    println!("{}", add(1, 2));
+    println!("{}", add_and_multiply(2, 3));
+    println!("{:?}", lt_gn(&test, &test));
 }
