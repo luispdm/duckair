8664eac60cb461ada1769557a01750d8a72469d9
luispdm <17044119+luispdm@users.noreply.github.com>
2023-09-05 22:46:10 +0200

    traits from the rust book

--- 11_generics/main.rs
+++ 11_generics/main.rs
@@ -1,4 +1,5 @@
 use std::ops::{Add, Mul};
+use std::fmt::Display;
 
 #[derive(Debug)]
 struct WithGenerics<T, U> {
@@ -9,7 +10,8 @@ struct WithGenerics<T, U> {
 
 /*
  * Generics identifiers defined here don't need to be the same as the ones
- * defined in the original struct (note X and Y here, while T and U were defined above).
+ * defined in the original struct (note X and Y here, T and U
+ * in the definition of the struct above).
  */
 impl<X, Y> WithGenerics<X, Y> {
     fn first(&self) -> &X {
@@ -39,17 +41,25 @@ impl<T, U> WithGenerics<T, U> {
 }
 
 // this tells the compiler that T must implement the trait "Add" in order to be used by the method
+// this is called "trait bound" (more at 99_rust_book/10_traits)
 fn add<T: Add<Output = T>>(a: T, b: T) -> T {
     a + b
 }
 
-// when a type must implement multiple traits, prefer the "where" clause
+// when trait bound defines multiple traits, prefer the "where" clause
 fn add_and_multiply<T>(a: T, b: T) -> T
 where
     T: Add<Output = T> + Mul<Output = T> + Copy,
 {
     let c = a + b;
     c * a * b
+}
+
+// another type of trait bound
+impl<T: Display, U> WithGenerics<T, U> {
+    fn trait_bound(&self) {
+        println!("{}", self.name)
+    }
 }
 
 // this is how you declare a method that uses both lifetime and generics
---
+++ 99_rust_book/10_traits/main.rs
@@ -0,0 +1,116 @@
+use std::fmt::Display;
+
+trait Summary {
+    // traits can have default implementations
+    fn summarize(&self) -> String {
+        String::from("read more...")
+    }
+    // default implementations can call other methods of the trait
+    fn summarize_with_author(&self) -> String {
+        format!("read more... from {}", self.author())
+    }
+    fn author(&self) -> &String;
+}
+
+struct Tweet {
+    pub username: String,
+    pub tweet: String,
+    pub replies: Vec<String>,
+}
+
+struct Article {
+    pub author: String,
+    pub headline: String,
+}
+
+impl Summary for Article {
+    fn author(&self) -> &String {
+        &self.author
+    }
+}
+
+impl Summary for Tweet {
+    // Only the implementations of Summary here.
+    // If you want to define other methods for Tweet,
+    // they must be wrapped inside another "impl Tweet".
+
+    // default implementations can be overridden
+    fn summarize(&self) -> String {
+        format!("{} tweeted: {}", self.username, self.tweet)
+    }
+    fn author(&self) -> &String {
+        &self.username
+    }
+}
+
+// This is how polymorphism is achieved in Rust.
+// It's syntactic sugar of what's below
+fn notify(item: &impl Summary) {
+    println!("Breaking news! {}", item.summarize());
+}
+// this syntax is called "trait bound"
+fn notify_tb<T: Summary>(item: &T) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+// Sometimes that syntactic sugar is not powerful enough.
+// What if I want arg1 and arg2 to be exactly of the same type?
+// This is something I can only specify with trait bounds
+fn not_same_type(arg1: &impl Summary, arg2: &impl Summary) {
+    
+}
+fn same_type<T: Summary>(arg1: &T, arg2: &T) {
+    
+}
+
+// more on trait bounds at 11_generics
+
+// polymorphism pt.2 - this is very useful in closures and iterators
+fn returns_summarizable(switch: bool) -> impl Summary {
+    if switch{
+        Article {
+            author: String::from("Deux Jean"),
+            headline: String::from("C'est la vie"),
+        }
+    } else {
+        /*
+         * The code below doesn't compile! The return type
+         * must be the same in all the function body.
+         * Different return types can be specified only
+         * via trait objects, which we will cover in chapter 17
+         */
+        // Tweet {
+        //     username: String::from("@johndoe"),
+        //     tweet: String::from("fake news... fake news everywhere!"),
+        //     replies: vec![],
+        // }
+        Article {
+            author: String::from("Juan Dos"),
+            headline: String::from("Ojalá!"),
+        }
+    }
+}
+
+/*
+ * Blanket implementation.
+ * Implement a trait on a type that implements another trait.
+ * It is widely used in the standard library.
+ * The below is an example from the standard library, in fact,
+ * it doesn't compile locally.
+ */
+// impl<T: Display> ToString for T {
+// }
+
+fn main() {
+    let article = Article {
+        author: String::from("John Pulitzer"),
+        headline: String::from("It's the end of the world as we know it"),
+    };
+    let tweet = Tweet {
+        username: String::from("@johndoe"),
+        tweet: String::from("fake news... fake news everywhere!"),
+        replies: vec![],
+    };
+    not_same_type(&article, &tweet)
+    // same_type(&article, &tweet); // doesn't compile!
+}
