4730f66719c1fae0c049bc96ca3522b229750b71
luispdm <17044119+luispdm@users.noreply.github.com>
2023-09-14 17:21:54 +0200

    return Result from new

--- 99_rust_book/12_cli_app/src/main.rs
+++ 99_rust_book/12_cli_app/src/main.rs
@@ -1,13 +1,17 @@
-use std::{env, fs};
+use std::{env, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("problem parsing arguments: {}", err);
+        process::exit(1)
+    });
     println!("searching for {}", config.query);
     println!("in {}", config.filename);
 
-    let content = fs::read_to_string(config.filename).expect("something went wrong reading the file");
+    let content =
+        fs::read_to_string(config.filename).expect("something went wrong reading the file");
     println!("with text:\n{}", content);
 }
 
@@ -17,13 +21,29 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    /*
+     * The rust book returns "Result<Config, &str>" and the error returned is "Err("not enought arguments")".
+     *
+     * The return type of the method has been changed to "Result<Config, String>" because I am creating a String
+     * via "format!". Once I do that, I am creating an owned type => "new" now owns that String => if I were to return
+     * the &str from that String, via ".as_str()" for example, I would be extracting a slice from the String => I would
+     * be referencing the value, i.e. borrow.
+     * This cannot be done because when the method exits, the String cease to exist, i.e. dangling reference!!!
+     *
+     * The Rust book can return "Err("not enought arguments")" because the string inside "Err" has been stored in the
+     * program binary, i.e. it is a "&'static str", its lifetime is bound to the program binary.
+     *
+     */
+    fn new(args: &[String]) -> Result<Config, String> {
         if args.len() < 3 {
-            panic!("not enought arguments: found {}, want 3", args.len());
+            return Err(format!(
+                "not enought arguments: found {}, want 3",
+                args.len()
+            ));
         }
         // clone not efficient because it copies data, but easier than using lifetimes for now
         let query = args[1].clone();
         let filename = args[2].clone();
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
