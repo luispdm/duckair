43347fcc6f895f5ac7a888a4ebfd2ae58e51d768
luispdm <17044119+luispdm@users.noreply.github.com>
2023-09-14 16:49:04 +0200

    move argument parsing to fn

--- 99_rust_book/12_cli_app/src/main.rs
+++ 99_rust_book/12_cli_app/src/main.rs
@@ -3,11 +3,16 @@ use std::{env, fs};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let (query, filename) = parse_config(&args);
     println!("searching for {}", query);
     println!("in {}", filename);
 
     let content = fs::read_to_string(filename).expect("something went wrong reading the file");
     println!("with text:\n{}", content);
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+    (query, filename)
 }
