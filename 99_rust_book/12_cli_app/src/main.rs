6fff4e5e924ba247dd222c5f94ef6741a7c6fc51
luispdm <17044119+luispdm@users.noreply.github.com>
2023-09-14 16:53:15 +0200

    group query & filename in struct

--- 99_rust_book/12_cli_app/src/main.rs
+++ 99_rust_book/12_cli_app/src/main.rs
@@ -3,16 +3,22 @@ use std::{env, fs};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (query, filename) = parse_config(&args);
-    println!("searching for {}", query);
-    println!("in {}", filename);
+    let config = parse_config(&args);
+    println!("searching for {}", config.query);
+    println!("in {}", config.filename);
 
-    let content = fs::read_to_string(filename).expect("something went wrong reading the file");
+    let content = fs::read_to_string(config.filename).expect("something went wrong reading the file");
     println!("with text:\n{}", content);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
-    (query, filename)
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    // clone not efficient because it copies data, but easier than using lifetimes for now
+    let query = args[1].clone();
+    let filename = args[2].clone();
+    Config { query, filename }
 }
