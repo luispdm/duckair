e9207b505562bd9a7462ec894c03f6a8eec35914
luispdm <17044119+luispdm@users.noreply.github.com>
2024-02-15 12:33:56 +0100

    advanced traits

--- 99_rust_book/19_2_advanced_traits/main.rs
+++ 99_rust_book/19_2_advanced_traits/main.rs
@@ -1,4 +1,7 @@
-use std::ops::Add;
+use std::{
+    fmt::{self, Debug},
+    ops::{Add, Deref},
+};
 
 fn main() {
     // associated types - they work similarly as generics
@@ -72,7 +75,123 @@ fn main() {
      * To summarize, you wanna use generic default type parameters for two reasons:
      * 1. extend a type without breaking existing code
      * 2. allow customization for specific cases (see Meters and Millimeters)
-    */
+     */
 
-    // TODO continue video on section: "Calling methods with the same name" at 7:17
+    // methods with the same name
+    // Rust allows you to implement multiple traits with the same method name, as well as implementing the
+    // same method on the type itself, without referencing any of the traits. how do you call
+    // each of the implementations? see the code below
+    trait Wizard {
+        fn fly(&self);
+    }
+    trait Pilot {
+        fn fly(&self);
+    }
+    struct Human {}
+    impl Pilot for Human {
+        fn fly(&self) {
+            println!("this is your captain speaking")
+        }
+    }
+    impl Wizard for Human {
+        fn fly(&self) {
+            println!("up!")
+        }
+    }
+    impl Human {
+        // implementing the same method name for the type itself, no trait involved this time
+        fn fly(&self) {
+            println!("*waving arms furiously*");
+        }
+    }
+    let human = Human {};
+    // human's own implementation is called
+    human.fly();
+    // pilot's implementation is called
+    Pilot::fly(&human);
+    // wizard's implementation is called
+    Wizard::fly(&human);
+    // you can do the same thing with associative functions
+    trait RotaryEngine {
+        fn accelerate();
+    }
+    trait JetEngine {
+        fn accelerate();
+    }
+    struct Car {}
+    impl Car {
+        fn accelerate() {
+            println!("turbo lag... go!");
+        }
+    }
+    impl RotaryEngine for Car {
+        fn accelerate() {
+            println!("*mazda 787b sound*");
+        }
+    }
+    impl JetEngine for Car {
+        fn accelerate() {
+            println!("*a pleasantly painful sound*");
+        }
+    }
+    Car::accelerate();
+    <Car as RotaryEngine>::accelerate();
+    <Car as JetEngine>::accelerate();
+
+    // supertraits
+    // sometimes traits depend on functionality of other traits: i.e. your trait depends on the other
+    // trait being implemented, that trait is called "supertrait".
+    // this is how you tell to a trait to depend on another trait
+    trait OutlinePrint: fmt::Display {
+        fn outline_print(&self) {
+            // without the addition to the trait, the compiler wouldn't be able to find "to_string()"
+            let out = self.to_string();
+            println!("{}", out);
+        }
+    }
+    // Point must implement fmt of the Display trait if it wants to implement OutlinePrint
+    impl fmt::Display for Point {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            write!(f, "({}, {})", self.x, self.y)
+        }
+    }
+    impl OutlinePrint for Point {
+        fn outline_print(&self) {
+            println!("{}", self.x);
+        }
+    }
+    // newtype pattern
+    // in chapter 10 we learned about the orphan rule, which states that we can implement a trait on a type
+    // as long as the type or the trait belong to our crate.
+    // the code below does not compile as both Display and String are defined outside of our crate:
+    // impl fmt::Display for Vec<String> {
+    //     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    //         write!(f, "")
+    //     }
+    // }
+    // with the newtype pattern, we can circumvent this restriction. i have to create a tuple struct
+    // wrapping the type I want to implement the trait for:
+    struct Wrapper(Vec<String>);
+    impl fmt::Display for Wrapper {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            write!(f, "[{}]", self.0.join(", "))
+        }
+    }
+    /*
+     * What's the downside of this approach? I cannot call the methods of Vec<String> directly on Wrapper.
+     * e.g.: "w.first();" does not compile, I have to do: "w.0.first();".
+     * There is a solution: I can implement the Deref trait for Wrapper such that dereferencing Wrapper returns
+     * Vec<String>. The code below allows me to do that:
+     */
+    impl Deref for Wrapper {
+        type Target = Vec<String>;
+        fn deref(&self) -> &Self::Target {
+            &self.0
+        }
+    }
+    let w = Wrapper(vec![String::from("first"), String::from("second")]);
+    if let Some(first) = w.first() {
+        println!("{first}");
+    }
+    // if I want to modify Vec<String> from Wrapper, I also have to implement DerefMut for Wrapper.
 }
